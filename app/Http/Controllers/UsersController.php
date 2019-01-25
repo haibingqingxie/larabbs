971c493a079050dcb63e11f6bdcf14f80b722ff5
haibingqingxie <jasonli@seasidecrab.com>
2019-01-25 03:40:49 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -19,7 +20,7 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $imageUploadHandler, User $user)
     {
         // $this->validate($request, [
         //     'name' => 'required|max:255',
@@ -32,8 +33,18 @@ class UsersController extends Controller
         // }
 
         // $user->save();
+        // dd($request->avatar);
+         $data = $request->all();
+         // dd($data);
 
-        $user->update($request->all());
+        if ($request->avatar) {
+            $result = $imageUploadHandler->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
 
         return redirect()->route('users.show', [$user->id])->with('success', '个人资料更新成功！');
     }
